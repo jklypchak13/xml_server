8e864f930dc89e42aeed407402f987e513bf1df6
jklypchak13 <jarod.klypchak@yahoo.com>
2025-11-25 17:21:51 -0600

    Add validation for xml files

--- main.py
+++ main.py
@@ -1,45 +1,8 @@
-from fastapi import FastAPI, Response
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import sessionmaker
-import os
+from fastapi import FastAPI
+import xml_server.xml_file
 
-db_url = str(os.getenv('DB_URL'))
 app = FastAPI()
-engine = create_engine(db_url)
-SessionLocal = sessionmaker(bind=engine, autoflush=False)
-
-@app.get('/xml_file')
-def get_files():
-    session = SessionLocal()
-
-    res = session.execute(text(
-        """SELECT id, xml_data from xmlfiles
-        """
-    ))
-
-    values = {}
-    for value in res.all():
-        values[value[0]] = value[1]
-    session.close()
-
-    return values
-
-
-@app.post('/xml_file')
-def xml_receive(xml_data:str):
-    # Do something with xml_str, e.g., store in DB
-    print(xml_data)
-
-    session = SessionLocal()
-
-    session.execute(text("""
-        INSERT INTO xmlfiles(xml_data)
-            VALUES('{}'::xml);
-    """.format(xml_data)))
-    session.commit()
-    session.close()
-    return Response(content="XML received", media_type="text/plain")
-
+app.include_router(xml_server.xml_file.router)
 
 @app.get('/')
 def root():
---
+++ xml_server/xml_file.py
@@ -0,0 +1,66 @@
+
+from fastapi import APIRouter, Response, HTTPException
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+import os
+import xml.etree.ElementTree as ET
+
+db_url = str(os.getenv('DB_URL'))
+router = APIRouter(prefix='/xml_file')
+engine = create_engine(db_url)
+SessionLocal = sessionmaker(bind=engine, autoflush=False)
+
+def validate_xml_str(data:str)->bool:
+    try:
+        # Can do more validation later
+        tree = ET.fromstring(data)
+    except Exception:
+        return False
+    return True
+
+@router.get('/')
+def get_xml_files()->dict:
+    session = SessionLocal()
+
+    res = session.execute(text(
+        """SELECT id, xml_data from xmlfiles
+        """
+    ))
+
+    values = {}
+    for value in res.all():
+        values[value[0]] = value[1]
+    session.close()
+
+    return values
+
+@router.get('/{id}')
+def get_xml_file_by_id(id:int)->dict:
+    session = SessionLocal()
+    res = session.execute(text(
+        """SELECT id, xml_data from xmlfiles WHERE id={}
+        """.format(id)
+    ))
+    values = {}
+    for value in res.all():
+        values[value[0]] = value[1]
+    session.close()
+
+    if len(values) == 0:
+        raise HTTPException(status_code=404, detail='record not found')
+
+    return values
+
+
+@router.post('/xml_file')
+def receive_xml_file(xml_data:str)->str:
+    if not validate_xml_str(xml_data):
+        raise HTTPException(status_code=400, detail='invalid xml data')
+    session = SessionLocal()
+    session.execute(text("""
+        INSERT INTO xmlfiles(xml_data)
+            VALUES('{}'::xml);
+    """.format(xml_data)))
+    session.commit()
+    session.close()
+    return 'Received'
